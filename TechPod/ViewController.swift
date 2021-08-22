7d7957928663f01deb49732ada81b84a46597a10
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2021-08-23 01:52:55 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by 河村大介 on 2021/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
